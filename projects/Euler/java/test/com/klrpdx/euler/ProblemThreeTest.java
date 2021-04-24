e1fd28549ecc55c84351cb4a71bbb5caaaaabf84
Ken Richards <krichards@jamasoftware.com>
2021-04-24 11:32:54 -0700

    master:  Project Euler changes

--- projects/Euler/java/test/com/klrpdx/euler/ProblemThreeTest.java
+++ projects/Euler/java/test/com/klrpdx/euler/ProblemThreeTest.java
@@ -1,5 +1,7 @@
 package com.klrpdx.euler;
 
+import com.klrpdx.util.Solver;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,6 +32,20 @@ class ProblemThreeTest {
         assertEquals(79, p3.solve(1027));
     }
 
+    @Test
+    void runTimes() {
+        ProblemThree p3 = new ProblemThree();
+
+        for(long i=100000L;i<10000000000L;i*=2) {
+            Solver solver = new Solver(p3);
+            Long result = (Long)solver.solve(i);
+            Double time = solver.time();
+            System.out.printf("Value: %d, Solution: %d, Time: %.2f%n", i, result, time);
+        }
+    }
+
+
+    @Disabled
     @Test
     void problemSolution() {
         ProblemThree p3 = new ProblemThree();
